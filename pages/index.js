790ab59d8ed9ef995d4bade058dc98641285283e
Jerome Haas <hello@jeromehaas.dev>
2021-09-04 22:06:49 +0200

    Setup: initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bday Site App</title>
+        <meta name="description" content="Overview of all friends birthdays" />
+      </Head>
+
+      <main>
+     		hello world
+      </main>
+
+    </div>
+  )
+}
